92b72f72cfa1ef37f7cfe4039ce502d0235e25f0
yeehui.poh <yhpoh91@gmail.com>
2021-10-02 16:02:10 +0800

    add support for linked in

--- src/router/index.js
+++ src/router/index.js
@@ -5,41 +5,48 @@ const { L } = require('kopitech-logger')('Global Router');
 
 const router = express.Router({ mergeParams: true });
 
+const getData = () => ({
+  vcard: {
+    url: `${process.env.HOST}/yeehuipoh/vcard`,
+  },
+  personal: {
+    firstName: process.env.MECARD_PERSONAL_FIRST_NAME,
+    lastName: process.env.MECARD_PERSONAL_LAST_NAME,
+  },
+
+  contact: {
+    email: process.env.MECARD_CONTACT_EMAIL,
+    phoneNumber: process.env.MECARD_CONTACT_PHONE_NUMBER,
+    wechat: (process.env.MECARD_CONTACT_WECHAT_ENABLED || 'true') === 'true' && {
+      handle: process.env.MECARD_CONTACT_WECHAT_HANDLE,
+      url: `weixin://dl/profile/${process.env.MECARD_CONTACT_WECHAT_HANDLE}`,
+    },
+    
+    linkedIn: (process.env.MECARD_CONTACT_LINKEDIN_ENABLED || 'true') === 'true' && {
+      handle: `@${process.env.MECARD_CONTACT_LINKEDIN_HANDLE}`,
+      url: `https://linkedin.com/in/${process.env.MECARD_CONTACT_LINKEDIN_HANDLE}`,
+    },
+    
+    github: (process.env.MECARD_CONTACT_GITHUB_ENABLED || 'true') === 'true' && {
+      handle: `@${process.env.MECARD_CONTACT_GITHUB_HANDLE}`,
+      url: `https://github.com/${process.env.MECARD_CONTACT_GITHUB_HANDLE}`,
+    },
+  },
+
+  career: {
+    company: {
+      name: process.env.MECARD_CAREER_COMPANY_NAME,
+      website: process.env.MECARD_CAREER_COMPANY_WEBSITE,
+    },
+    position: process.env.MECARD_CAREER_POSITION,
+  },
+});
+
 const controller = async (req, res, next) => {
   try {
     const template = "taby-portrait";
 
-    const me = {
-      vcard: {
-        url: `${process.env.HOST}/yeehuipoh/vcard`,
-      },
-      personal: {
-        firstName: process.env.MECARD_PERSONAL_FIRST_NAME,
-        lastName: process.env.MECARD_PERSONAL_LAST_NAME,
-      },
-
-      contact: {
-        email: process.env.MECARD_CONTACT_EMAIL,
-        phoneNumber: process.env.MECARD_CONTACT_PHONE_NUMBER,
-        wechat: (process.env.MECARD_CONTACT_WECHAT_ENABLED || 'true') === 'true' && {
-          handle: process.env.MECARD_CONTACT_WECHAT_HANDLE,
-          url: `weixin://dl/profile/${process.env.MECARD_CONTACT_WECHAT_HANDLE}`,
-        },
-        
-        github: (process.env.MECARD_CONTACT_GITHUB_ENABLED || 'true') === 'true' && {
-          handle: `@${process.env.MECARD_CONTACT_GITHUB_HANDLE}`,
-          url: `https://github.com/${process.env.MECARD_CONTACT_GITHUB_HANDLE}`,
-        },
-      },
-
-      career: {
-        company: {
-          name: process.env.MECARD_CAREER_COMPANY_NAME,
-          website: process.env.MECARD_CAREER_COMPANY_WEBSITE,
-        },
-        position: process.env.MECARD_CAREER_POSITION,
-      },
-    };
+    const me = getData();
     res.render(template, me);
   } catch (error) {
     L.error(error);
@@ -49,35 +56,8 @@ const controller = async (req, res, next) => {
 
 const vcardController = async (req, res, next) => {
   try {
-    const me = {
-      personal: {
-        firstName: process.env.MECARD_PERSONAL_FIRST_NAME,
-        lastName: process.env.MECARD_PERSONAL_LAST_NAME,
-      },
-
-      contact: {
-        email: process.env.MECARD_CONTACT_EMAIL,
-        phoneNumber: process.env.MECARD_CONTACT_PHONE_NUMBER,
-        wechat: (process.env.MECARD_CONTACT_WECHAT_ENABLED || 'true') === 'true' && {
-          handle: process.env.MECARD_CONTACT_WECHAT_HANDLE,
-          url: `weixin://dl/profile/${process.env.MECARD_CONTACT_WECHAT_HANDLE}`,
-        },
-        
-        github: (process.env.MECARD_CONTACT_GITHUB_ENABLED || 'true') === 'true' && {
-          handle: `@${process.env.MECARD_CONTACT_GITHUB_HANDLE}`,
-          url: `https://github.com/${process.env.MECARD_CONTACT_GITHUB_HANDLE}`,
-        },
-      },
-
-      career: {
-        company: {
-          name: process.env.MECARD_CAREER_COMPANY_NAME,
-          website: process.env.MECARD_CAREER_COMPANY_WEBSITE,
-        },
-        position: process.env.MECARD_CAREER_POSITION,
-      },
-    };
-
+    
+    const me = getData();
     const vcard = new VCard();
     vcard
       .addName(me.personal.lastName, me.personal.firstName)
@@ -85,9 +65,13 @@ const vcardController = async (req, res, next) => {
       .addPhoneNumber(me.contact.phoneNumber)
       .addCompany(me.career.company.name)
       .addJobtitle(me.career.position)
+      .addURL(me.contact.linkedIn.url, "HOME")
       .addURL(me.career.company.website, "WORK")
 
-    res.set({ "Content-Disposition": `attachment; filename=\"${me.personal.firstName}_${me.personal.lastName}_vcard.vcf\"` });
+    res.set({
+      "Content-Disposition": `attachment; filename=\"${me.personal.firstName}_${me.personal.lastName}_vcard.vcf\"`,
+      "Content-Type": "text/vcard"
+    });
     res.send(vcard.toString());
   } catch (error) {
     L.error(error);
